65040900d7eaada2159842457cc9b9bfe6b4e1ae
Héctor C. Alzate <halzate93@gmail.com>
2017-02-07 14:57:55 -0500

    Refactored to use a different file for the handling

---
+++ maze.js
@@ -0,0 +1,8 @@
+var generateMaze = function (request, response)
+{
+  //TODO: Generar el maze
+  response.write ("Hello");
+  response.end ();
+};
+
+exports.generateMaze = generateMaze;
--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var http = require ('http');
+var maze = require ('./maze');
 
-var handle = function (request, response)
-{
-  response.write ("Hello");
-  response.end ();
-};
-
-var server = http.createServer (handle);
+var server = http.createServer (maze.generateMaze);
 var port = process.env.PORT || 3000;
 
 console.log ("Start listening");
