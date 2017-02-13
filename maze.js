f30ee0fb77fa138021511ff46ce368fb647d8090
Héctor C. Alzate <halzate93@gmail.com>
2017-02-13 00:32:47 -0500

    Render maze with ascii

--- maze.js
+++ maze.js
@@ -6,18 +6,15 @@ var generateMaze = function (request, response)
 {
   var query = request.url.split ("?")[1];
   var data = url_parser.parse (query);
-  var x = data.x | 5;
-  var y = data.y | 5;
+  var x = data.x;
+  var y = data.y;
 
   var maze = getMaze (x, y);
-  response.write (toString (maze));
+  maze.render = toAscii (maze);
+  console.log (maze.render);
+  response.write (maze.render);
   response.end ();
 };
-
-function toString (maze)
-{
-  return JSON.stringify (maze);
-}
 
 function getMaze (x, y)
 {
@@ -25,4 +22,51 @@ function getMaze (x, y)
   return maze;
 }
 
+function toAscii (maze)
+{
+  var render = "";
+  for (var y=0; y<maze.length; y++)
+  {
+    render += drawTop (maze[y]) + "\n";
+    render += drawSides (maze[y]) + "\n";
+  }
+  render += drawBottom (maze[maze.length - 1]) + "\n";
+  return render;
+}
+
+function drawTop (row)
+{
+  var top = "";
+  for (x=0; x<row.length; x++)
+  {
+    top += "+";
+    top += row[x].top? "---" : "   ";
+  }
+  top += "+";
+  return top;
+}
+
+function drawSides (row)
+{
+  var sides = "";
+  for (var x=0; x<row.length; x++)
+  {
+    sides += row[x].left? "|" : " ";
+    sides += "   ";
+  }
+  sides += row[row.length - 1].right? "|" : " ";
+  return sides;
+}
+
+function drawBottom (row)
+{
+  var bottom = "";
+  for (var x=0; x<row.length; x++)
+  {
+    bottom += "+";
+    bottom += row[x].bottom? "---" : "   ";
+  }
+  bottom += "+";
+  return bottom;
+}
 exports.generateMaze = generateMaze;
