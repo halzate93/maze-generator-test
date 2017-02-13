2756074013e7e13725381dc6c9217af4a5915067
Héctor C. Alzate <halzate93@gmail.com>
2017-02-13 00:02:16 -0500

    Parsing request data

--- maze.js
+++ maze.js
@@ -1,8 +1,28 @@
+var generator = require('generate-maze');
+var body_parser = require ('body/json');
+var url_parser = require ('querystring');
+
 var generateMaze = function (request, response)
 {
-  //TODO: Generar el maze
-  response.write ("Hello");
+  var query = request.url.split ("?")[1];
+  var data = url_parser.parse (query);
+  var x = data.x | 5;
+  var y = data.y | 5;
+
+  var maze = getMaze (x, y);
+  response.write (toString (maze));
   response.end ();
 };
+
+function toString (maze)
+{
+  return JSON.stringify (maze);
+}
+
+function getMaze (x, y)
+{
+  var maze = generator(x, y);
+  return maze;
+}
 
 exports.generateMaze = generateMaze;
