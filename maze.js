8cf9776ec1fc160ea052b64bc981d56804c5d49c
Héctor C. Alzate <halzate93@gmail.com>
2017-02-13 01:03:35 -0500

    Generate mazes

--- maze.js
+++ maze.js
@@ -1,24 +1,34 @@
 var generator = require('generate-maze');
-var body_parser = require ('body/json');
 var url_parser = require ('querystring');
 
 var generateMaze = function (request, response)
 {
   var query = request.url.split ("?")[1];
   var data = url_parser.parse (query);
-  var x = data.x;
-  var y = data.y;
+  var x = data.x || 5;
+  var y = data.y || 5;
 
   var maze = getMaze (x, y);
-  maze.render = toAscii (maze);
   console.log (maze.render);
-  response.write (maze.render);
-  response.end ();
+
+  var isRender = data.render || false;
+
+  if (isRender)
+    response.end (maze.render);
+  else
+  {
+    response.setHeader ("Content-Type", "application/json");
+    response.end (JSON.stringify(maze));
+  }
 };
 
 function getMaze (x, y)
 {
-  var maze = generator(x, y);
+  var grid = generator(x, y);
+  var maze = {
+    data: grid,
+    render: toAscii (grid)
+  };
   return maze;
 }
 
