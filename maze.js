38c2c6e031df04699f2b6ba89ed22f43a88b7913
Héctor C. Alzate <halzate93@gmail.com>
2017-02-13 13:51:19 -0500

    Cache maze for a day

--- maze.js
+++ maze.js
@@ -1,6 +1,7 @@
 var generator = require('generate-maze');
 var url_parser = require ('querystring');
 var toAscii = require ('./toAscii.js');
+var fs = require ('fs');
 
 var generateMaze = function (request, response)
 {
@@ -9,21 +10,61 @@ var generateMaze = function (request, response)
   var x = data.x || 5;
   var y = data.y || 5;
 
-  var maze = getMaze (x, y);
-  console.log (maze.render);
-
-  var isRender = data.render || false;
-
-  if (isRender)
-    response.end (maze.render);
-  else
-  {
-    response.setHeader ("Content-Type", "application/json");
-    response.end (JSON.stringify(maze));
-  }
+  getMaze (x, y, function (maze) {
+    console.log (maze.render);
+    if (data.render)
+      response.end (maze.render);
+    else
+    {
+      response.setHeader ("Content-Type", "application/json");
+      response.end (JSON.stringify(maze));
+    }
+  });
 };
 
-function getMaze (x, y)
+function getMaze (x, y, onFinished)
+{
+  loadMaze (function (maze, date) {
+    if (!maze || checkExpiration (date))
+    {
+      maze = buildNewMaze (x, y);
+      saveMaze (maze);
+    }
+    onFinished (maze);
+  });
+}
+
+function loadMaze (onLoaded)
+{
+  fs.readFile('/tmp/maze', (err, data) => {
+    if (err)
+      console.log ("Couldn't load maze");
+    else
+      console.log ("Loaded maze");
+      var stats = fs.statSync("/tmp/maze");
+      var mtime = new Date(stats.mtime);
+    onLoaded (JSON.parse (data), mtime);
+  });
+}
+
+function checkExpiration (date)
+{
+  return date.getDate () != new Date ().getDate ();
+}
+
+function saveMaze (maze)
+{
+  console.log (maze.render);
+  fs.writeFile("/tmp/maze", JSON.stringify (maze), function(err) {
+      if(err)
+      {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+  });
+}
+
+function buildNewMaze (x, y)
 {
   var grid = generator(x, y);
   var maze = {
